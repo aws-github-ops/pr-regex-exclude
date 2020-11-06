b95c31f6458979ac7182c24945737789ca6f0284
Tom Keller <1083460+kellertk@users.noreply.github.com>
2020-11-05 20:03:36 -0800

    Fix tests and package for release (#10)

--- test/process-diff.test.ts
+++ test/process-diff.test.ts
@@ -1,11 +1,12 @@
 import * as nock from 'nock';
 import * as path from 'path';
-import {processDiff} from '../src/process-diff';
+import {processDiffUrl} from '../src/process-diff';
 
 it('should fetch and process a diff', async () => {
   nock.load(path.join(__dirname, 'process-diff.http.json'));
-  const files = await processDiff(
-    'https://patch-diff.githubusercontent.com/raw/octocat/Hello-World/pull/602.diff'
+  const files = await processDiffUrl(
+    'https://patch-diff.githubusercontent.com/raw/octocat/Hello-World/pull/602.diff',
+    ''
   );
   expect(files).toEqual(['README']);
   nock.restore();
@@ -14,8 +15,9 @@ it('should fetch and process a diff', async () => {
 it('should throw an error for bad URLs', async () => {
   nock.load(path.join(__dirname, 'process-diff-err.http.json'));
   await expect(
-    processDiff(
-      'https://patch-diff.githubusercontent.com/raw/octocat/Hello-World/pull/LOL404.diff'
+    processDiffUrl(
+      'https://patch-diff.githubusercontent.com/raw/octocat/Hello-World/pull/LOL404.diff',
+      ''
     )
   ).rejects.toThrow();
   nock.restore();
